2e4eca94abb6f09b2e9c1dc0ac71dd616f9e933b
hari05-git <hariharankannan.005@gmail.com>
2026-02-06 04:34:25 +0000

    Update index.php

--- app/index.php
+++ app/index.php
@@ -8,20 +8,25 @@ $server_ip = $_SERVER['SERVER_ADDR'] ?? gethostbyname(gethostname());
   <style>
     body {
       font-family: Arial, sans-serif;
-      background-color: #ffffff;
+      background-color: #0f172a;
+      color: #e5e7eb;
       margin: 40px;
     }
     .card {
-      border: 1px solid #ccc;
+      border: 1px solid #334155;
       padding: 20px;
       border-radius: 10px;
       max-width: 500px;
+      background-color: #020617;
+    }
+    h1 {
+      color: #38bdf8;
     }
   </style>
 </head>
 <body>
   <div class="card">
-    <h1>Welcome to Streamline - v1</h1>
+    <h1>Welcome to StreamLine - v2 [New Feature]</h1>
     <p><strong>Server IP Address:</strong> <?php echo $server_ip; ?></p>
   </div>
 </body>
